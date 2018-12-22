75790c9d3185ce57f477deecc2e01fa0aba02a19
voipserver <voipserver@bz.com>
2018-12-22 19:10:14 +0330

    deleted:    install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-mvn package
-
-mkdir -p /opt/silo
-cp ./target/silo-0.0.2-SNAPSHOT.jar /opt/silo/silo.jar
-cp silo.yaml /opt/silo/silo.yaml
-cp ./script/initd /etc/init.d/silo
-chmod 755 /etc/init.d/silo
-update-rc.d silo defaults
